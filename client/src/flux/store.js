ad0f43b42eb0425a50ed3a43d37208c9414e2edf
phillipshalim <phillipshalim@gmail.com>
2020-10-14 04:54:05 -0700

    changed redux store

--- client/src/flux/store.js
+++ client/src/flux/store.js
@@ -2,17 +2,19 @@ import { createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
+
 const initialState = {};
 
-const middleWare = [thunk];
+const middleware = [thunk];
+
+const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
 
 const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+rootReducer,
+initialState,
+enhancer
 );
 
 export default store;
